2a977639049ce5ec57c42d2f7a4baf6dfea0c8ac
EarnPlayApps <EarnPlayApps@users.noreply.github.com>
2026-09-03 06:08:54 +0800

    Add AdMob load error diagnostics

--- app/src/main/java/com/admobtest/MainActivity.java
+++ app/src/main/java/com/admobtest/MainActivity.java
@@ -3,11 +3,14 @@ package com.admobtest;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.CookieManager;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,16 +21,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         WebView webView = findViewById(R.id.webView);
-
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
         CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
 
         webView.loadUrl("file:///android_asset/index.html");
 
-        MobileAds.initialize(this, initializationStatus -> {});
+        MobileAds.initialize(this, initializationStatus -> {
 
-        AdView adView = findViewById(R.id.adView);
-        adView.loadAd(new AdRequest.Builder().build());
+            AdView adView = findViewById(R.id.adView);
+
+            adView.setAdListener(new AdListener() {
+
+                @Override
+                public void onAdLoaded() {
+                    Toast.makeText(
+                        MainActivity.this,
+                        "AdMob: IKLAN BERJAYA LOAD",
+                        Toast.LENGTH_LONG
+                    ).show();
+                }
+
+                @Override
+                public void onAdFailedToLoad(LoadAdError error) {
+                    Toast.makeText(
+                        MainActivity.this,
+                        "AdMob ERROR: " + error.getCode() + " - " + error.getMessage(),
+                        Toast.LENGTH_LONG
+                    ).show();
+                }
+            });
+
+            AdRequest request = new AdRequest.Builder().build();
+            adView.loadAd(request);
+        });
     }
 }
