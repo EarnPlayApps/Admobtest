bf93dd9f964b390c772a85d55daf5ceb0e07d44c
EarnPlayApps <EarnPlayApps@users.noreply.github.com>
2026-09-03 03:01:24 +0800

    Fix Android project structure and workflow

---
+++ app/src/main/java/com/admobtest/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.admobtest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var adView: AdView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this) {}
+
+        adView = findViewById(R.id.adView)
+
+        val adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
+    }
+
+    override fun onDestroy() {
+        adView.destroy()
+        super.onDestroy()
+    }
+}
